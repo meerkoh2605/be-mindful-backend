9b72adf7271c4233d7f34719def167526fa1f1b3
Imac <imac@Imacs-iMac.local>
2026-05-08 23:16:59 +0500

    Initial Be Mindful backend

---
+++ server.js
@@ -0,0 +1,34 @@
+
+const express = require("express");
+const app = express();
+
+const articles = require("./data.json");
+
+const PORT = 3000;
+
+// HOME PAGE (this is where your /home goes)
+app.get("/home", (req, res) => {
+  let html = "<h1>Be Mindful 🧠</h1>";
+  html += "<h2>Articles</h2>";
+
+  articles.forEach(a => {
+    html += `<p><a href="/articles/${a.slug}">${a.title}</a></p>`;
+  });
+
+  res.send(html);
+});
+
+// API routes
+app.get("/articles", (req, res) => {
+  res.json(articles);
+});
+
+app.get("/articles/:slug", (req, res) => {
+  const article = articles.find(a => a.slug === req.params.slug);
+  res.json(article);
+});
+
+// START SERVER (must be LAST)
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
